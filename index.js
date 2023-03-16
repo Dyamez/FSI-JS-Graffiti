89f18bf2520277b6239f7798663e345950581d43
James Manansala <edward.dyamez@outlook.com>
2023-03-16 16:12:36 -0700

    031623_3

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 let header = document.querySelector('#page-header')
 header.style.textAlign = "left"
 
+
 let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i <  dogImages.length; i++) {
-    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.borderRadius = '400px'
+    dogImages[i].style.transform = 'scaleY(1)'
 }
+
+//dogImages.style.transform = "scaleY(1)"
+
+let footer = document.querySelector('.footer')
+footer.style.borderStyle = 'solid'
+
+let dogName = document.querySelector('.dog-name')
+dogName.style.textAlign = "left"
+
+header.style.color = 'blue'
+
+header.style.fontFamily ='timesNewRoman'
+
 /*
 let dogName = document.querySelectorAll('.dog-name')
-dogName.style.textAlign = "left"
-/*
+for (let i = 0; i < dogName.length; i++) {
+    let = dogName[i].style.textAlign = 'left'
+}
 
-// Select some elements...
+dogName.style.textAlign = "left"
+*/
+
+
+
+
